bac7b60d5203f63d5dcaea61eb8fd70d83bf34bf
kengchayodom <chayodom_j@gmail.com>
2025-09-04 15:51:04 +0700

    Lab07-3.1-Get-method

--- src/main/java/se331/lab/controller/EventController.java
+++ src/main/java/se331/lab/controller/EventController.java
@@ -3,6 +3,7 @@ package se331.lab.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import se331.lab.entity.Event;
 
 import jakarta.annotation.PostConstruct;
@@ -91,8 +92,15 @@ public class EventController {
     }
 
     @GetMapping("events")
-    public ResponseEntity<?> getEventLists(){
-        return ResponseEntity.ok(eventList);
+    public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false ) Integer perPage,@RequestParam(value = "_page", required = false ) Integer page) {
+        perPage = perPage == null?eventList.size():perPage;
+        page = page == null?1:page;
+        Integer firstIndex = (page-1)*perPage;
+        List<Event> output = new ArrayList<>();
+        for (int i = firstIndex; i < firstIndex + perPage; i++) {
+            output.add(eventList.get(i));
+        }
+        return ResponseEntity.ok(output);
     }
 
 }

78a3bbce5dce36aa0e7b63230b671854ec0163cf
kengchayodom <chayodom_j@gmail.com>
2025-09-04 17:39:49 +0700

    Lab07-4.4

--- src/main/java/se331/lab/Application.java
+++ src/main/java/se331/lab/Application.java
@@ -18,8 +18,12 @@ public class Application {
 		return new WebMvcConfigurer() {
 			@Override
 			public  void  addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/events").allowedOrigins("http://localhost:5173");
+				registry.addMapping("/events")
+						.allowedOrigins("http://localhost:5173");
+				registry.addMapping("/events/*")
+						.allowedOrigins("http://localhost:5173");
 			}
+
 		};
 	}
 }
